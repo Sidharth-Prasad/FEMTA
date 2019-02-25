10148029b97b43a37108ea28924b647a1c4f0d70
Noah Franks <noah@pal-nat186-79-100.itap.purdue.edu>
2019-02-25 16:16:11 -0500

    committing laptop state

--- spacebound/blue-origin/fake-data.sh
+++ spacebound/blue-origin/fake-data.sh
@@ -1,4 +1,3 @@
-
 echo "t:Fake Data Stream"
 echo "a:Axis 0"
 echo "a:Axis 1"
@@ -10,8 +9,8 @@ for i in $(seq 0 10240); do
 
     
     x=$(echo "s($i/180 + 0*$pi/3.0)" | bc -l)
-    y=$(echo "s($i/180 + 1*$pi/3.0)" | bc -l)
-    z=$(echo "s($i/180 + 2*$pi/3.0)" | bc -l)
+    y=$(echo "s($i/120 + 1*$pi/3.0)" | bc -l)
+    z=$(echo "s($i/90 + 2*$pi/3.0)" | bc -l)
     
     echo "d:$x $y $z"
     #sleep 0.001
